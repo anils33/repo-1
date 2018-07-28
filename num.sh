16942302685400b2188f3e2e53a74f40d7b7860a
anils33 <40775586+anils33@users.noreply.github.com>
2018-07-29 00:39:48 +0530

    Update num.sh

--- num.sh
+++ num.sh
@@ -1,2 +1,6 @@
-hi this is adding two no. in shell scripting
+#!/bin/bash
 
+# example of using arguments to a script
+echo "My first name is $1"
+echo "My surname is $2"
+echo "Total number of arguments is $#" 
